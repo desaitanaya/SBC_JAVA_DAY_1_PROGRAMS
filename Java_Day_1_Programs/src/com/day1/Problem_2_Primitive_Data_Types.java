bc4f7afaf56711e0b02b73c5acdba9d6901073fe
Tanaya <desaitanaya17@gmail.com>
2023-04-04 13:37:49 +0530

    [Tanaya] Add : This Java Program is to display the default value of all Primitive Data Types.

---
+++ Java_Day_1_Programs/src/com/day1/Problem_2_Primitive_Data_Types.java
@@ -0,0 +1,25 @@
+package com.day1;
+
+public class Problem_2_Primitive_Data_Types {
+	static int a;
+	static float b;
+	static double c;
+	static long d;
+	static short e;
+	static String str;
+	static byte by;
+	static boolean bool;
+	public static void main (String[] args) {
+		System.out.println("PROGRAM TO DISPLAY DEFAULT VALUE OF PRIMITIVE DATA TYPES");
+		System.out.println("Default value of Int : " +a);
+		System.out.println("Default value of Float : " +b);
+		System.out.println("Default value of Double : " +c);
+		System.out.println("Default value of Long : " +d);
+		System.out.println("Default value of Short : " +e);
+		System.out.println("Default value of Byte : " +by);
+		System.out.println("Default value of String : " +str);
+		System.out.println("Default value of Boolean : " +bool);
+		
+		
+	}
+}
