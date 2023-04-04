334fa6e1729cdebc298ea54d29ca78f1befbb0d9
Tanaya <desaitanaya17@gmail.com>
2023-04-04 13:37:01 +0530

    [Tanaya] Add : This Java Program is to display any message

---
+++ Java_Day_1_Programs/src/com/day1/Problem_1_Display_Message.java
@@ -0,0 +1,14 @@
+package com.day1;
+
+import java.util.Scanner;
+
+public class Problem_1_Display_Message {
+	String str;
+	public static void main (String[] args) {
+	System.out.println("PROGRAM TO DISPLAY ANY MESSAGE")
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter any message : ");
+	str=sc.nextLine();
+	System.out.println(str);
+	}
+}
